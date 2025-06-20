1c16886d031ecad7d7e28e5e3cf583b8562004cc
Dennis Opoola <161707811+Dennis00000@users.noreply.github.com>
2025-06-21 00:05:55 +0300

    Fix Metro configuration for web compatibility

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Configure resolver to provide web-compatible shims for native-only modules
-config.resolver.extraNodeModules = {
+config.resolver.alias = {
   'react-native/Libraries/Utilities/codegenNativeCommands': require.resolve('./metro-shims/codegenNativeCommands.js'),
 };
 
